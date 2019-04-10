8d1953807c17d7094c06253e59542b2c25e46e72
Chris Norman <chris.norman2@googlemail.com>
2019-04-10 14:42:15 +0100

    Start logging.

--- examples/empty.py
+++ examples/empty.py
@@ -1,8 +1,11 @@
 """An empty game which simply listens."""
 
+from logging import basicConfig
+
 from mudmaker import Game
 
 game = Game()
 
 if __name__ == '__main__':
+    basicConfig(level='INFO')
     game.run()
