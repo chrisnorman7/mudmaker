e49d161c34aa5374f8a505b4fb62af78cd69ac60
Chris Norman <chris.norman2@googlemail.com>
2019-04-17 20:16:55 +0100

    Added connection fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from pytest import fixture
 
-from mudmaker import Exit, Game, Room, Zone
+from autobahn.twisted import WebSocketServerFactory
+
+from mudmaker import Exit, Game, Room, Zone, WebSocketConnection
 
 
 @fixture(name='exit')
@@ -32,3 +34,48 @@ def get_room(game, zone):
 @fixture(name='zone')
 def get_zone(game):
     return game.make_object('Zone', (Zone,), name='Test Zone')
+
+
+class PretendPeer:
+    host = 'test.example.com'
+    port = 1234
+
+
+class PretendReason:
+    def getErrorMessage(self):
+        return 'Test conection was disconnected.'
+
+
+class PretendTransport:
+    def setTcpNoDelay(self, value):
+        pass
+
+    def getPeer(self):
+        return PretendPeer()
+
+
+class PretendConnection(WebSocketConnection):
+    """A pretend connection."""
+
+    def __init__(self, *args, **kwargs):
+        self.transport = PretendTransport()
+        super().__init__(*args, **kwargs)
+        self.last_message = ''
+
+    def send(self, *args, **kwargs):
+        pass
+
+    def message(self, string):
+        self.last_message = string
+        return super().message(string)
+
+
+@fixture(name='connection')
+def connection(game):
+    """Provides a pretend conection object."""
+    game.websocket_factory = WebSocketServerFactory()
+    game.websocket_factory.game = game
+    con = PretendConnection()
+    con.factory = game.websocket_factory
+    con.onOpen()
+    return con
