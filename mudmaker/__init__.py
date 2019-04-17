cf001026901f5b77c710b05ea2bc0f795af2a78f
Chris Norman <chris.norman2@googlemail.com>
2019-04-17 20:18:05 +0100

    Import WebSocketConnection class into module namespace.

--- mudmaker/__init__.py
+++ mudmaker/__init__.py
@@ -6,5 +6,9 @@ from .exits import Exit
 from .game import Game
 from .zones import Zone
 from .rooms import Room
+from .websockets import WebSocketConnection
 
-__all__ = ['Attribute', 'text', 'Direction', 'Exit', 'Game', 'Zone', 'Room']
+__all__ = [
+    'Attribute', 'text', 'Direction', 'Exit', 'Game', 'Zone', 'Room',
+    'WebSocketConnection'
+]
