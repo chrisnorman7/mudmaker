d57b19db8ede21a2843b8aea433eb3b8e01071f9
Chris Norman <chris.norman2@googlemail.com>
2019-04-25 17:45:25 +0100

    Added a test.

--- tests/socials_test.py
+++ tests/socials_test.py
@@ -1,3 +1,7 @@
+from mudmaker.parsers import main_parser
+from mudmaker.socials import Social
+
+
 def test_socials_factory(game, socials):
     assert game.socials_factory is socials
 
@@ -8,3 +12,25 @@ def test_name(obj, socials):
 
 def test_ss(obj, socials):
     assert socials.suffixes['ss'](obj, 'ss') == ('your', f"{obj.name}'s")
+
+
+def test_social():
+    name = 'test'
+    first = '%1N test%1s.'
+    second = '%1N test%1s something.'
+    third = '%1N test%1s %2n.'
+    description = 'Test something.'
+    s = main_parser.social(name, first, second, third, description=description)
+    assert isinstance(s, Social)
+    for cmd in main_parser.commands:
+        if cmd.name == name:
+            f = cmd.func
+            assert f.func is s
+            assert f.args == ['player', 'target']
+            break
+    else:
+        raise RuntimeError('Command not found.')
+    assert s.no_target == first
+    assert s.self_target == second
+    assert s.any_target == third
+    assert s.__doc__ == description
